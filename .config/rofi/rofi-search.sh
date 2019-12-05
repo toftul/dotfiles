5e74c9a7a854ddb6c9e929de6ed774f03005d7fb
Ivan <toftul.ivan@gmail.com>
2019-12-06 00:31:41 +0300

    On branch master Your branch is up to date with 'origin/master'. Changes to be committed: 	modified:   .config/rofi/rofi-search.sh

--- .config/rofi/rofi-search.sh
+++ .config/rofi/rofi-search.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-xdg-open "$(rg --no-messages --files ~/Documents \
-    ~/Downloads ~/Music ~/Videos ~/gdrive ~/gdriveShared \
-    -g "!{*.srt,*.rar,*.txt,*.zip,*.nfo}" | rofi -threads 0 \
-    -dmenu -sort -sorting-method fzf -i -p ">>>")"
+if [[ "$(hostname)" == "core" ]]; then
+    xdg-open "$(rg --no-messages --files ~/Documents \
+        ~/Downloads ~/Music ~/Videos ~/data/gdrive ~/data/gdriveShared \
+        -g "!{*.srt,*.rar,*.txt,*.zip,*.nfo}" | rofi -threads 0 \
+        -dmenu -sort -sorting-method fzf -i -p ">>>")"
+else if [[ "$(hostname)" == "arch" ]]; then
+    xdg-open "$(rg --no-messages --files ~/Documents \
+        ~/Downloads ~/Music ~/Videos ~/gdrive ~/gdriveShared \
+        -g "!{*.srt,*.rar,*.txt,*.zip,*.nfo}" | rofi -threads 0 \
+        -dmenu -sort -sorting-method fzf -i -p ">>>")"
+else
+    xdg-open "$(rg --no-messages --files ~/Documents \
+        ~/Downloads ~/Music ~/Videos ~/gdrive ~/gdriveShared \
+        -g "!{*.srt,*.rar,*.txt,*.zip,*.nfo}" | rofi -threads 0 \
+        -dmenu -sort -sorting-method fzf -i -p ">>>")"
+fi
+
