dfe4c194658862baac130d0901caf3492698353b
Ivan <toftul.ivan@gmail.com>
2019-12-06 17:27:41 +0300

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   .config/ranger/rc.conf##hostname.arch
    	modified:   .config/rofi/rofi-search.sh

--- .config/rofi/rofi-search.sh
+++ .config/rofi/rofi-search.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 if [[ "$(hostname)" == "core" ]]; then
     xdg-open "$(rg --no-messages --files ~/Documents \
         ~/Downloads ~/Music ~/Videos ~/data/gdrive ~/data/gdriveShared \
         -g "!{*.srt,*.rar,*.txt,*.zip,*.nfo}" | rofi -threads 0 \
         -dmenu -sort -sorting-method fzf -i -p ">>>")"
-else if [[ "$(hostname)" == "arch" ]]; then
+elif [[ "$(hostname)" == "arch" ]]; then
     xdg-open "$(rg --no-messages --files ~/Documents \
         ~/Downloads ~/Music ~/Videos ~/gdrive ~/gdriveShared \
         -g "!{*.srt,*.rar,*.txt,*.zip,*.nfo}" | rofi -threads 0 \
