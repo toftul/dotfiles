c90d88992a644631034a73d629f93d7f4ffbcfd9
Ivan <toftul.ivan@gmail.com>
2019-12-07 14:01:40 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	renamed:    .config/ranger/rc.conf##hostname.arch -> .config/ranger/rc.conf##default 	modified:   .config/rofi/rofi-search.sh

--- .config/rofi/rofi-search.sh
+++ .config/rofi/rofi-search.sh
@@ -5,6 +5,11 @@ if [[ "$(hostname)" == "core" ]]; then
         ~/Downloads ~/Music ~/Videos ~/data/gdrive ~/data/gdriveShared \
         -g "!{*.srt,*.rar,*.txt,*.zip,*.nfo}" | rofi -threads 0 \
         -dmenu -sort -sorting-method fzf -i -p ">>>")"
+elif [[ "$(hostname)" == "metalab-desktop" ]]; then
+    xdg-open "$(rg --no-messages --files ~/Documents \
+        ~/Downloads ~/Music ~/Videos ~/gdrive ~/gdriveShared \
+        -g "!{*.srt,*.rar,*.txt,*.zip,*.nfo}" | rofi -threads 0 \
+        -dmenu -sort -sorting-method fzf -i -p ">>>")"
 elif [[ "$(hostname)" == "arch" ]]; then
     xdg-open "$(rg --no-messages --files ~/Documents \
         ~/Downloads ~/Music ~/Videos ~/gdrive ~/gdriveShared \
