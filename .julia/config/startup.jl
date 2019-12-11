e2cd33ee9d54ab392174cd4ca899d521dcfec3a1
Ivan <toftul.ivan@gmail.com>
2019-12-11 18:17:06 +0300

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   .julia/config/startup.jl

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,12 +1,12 @@
 if gethostname() == "arch"
     push!(
           LOAD_PATH, 
-          "/home/ivan/gdrive/science/research/projects/julia-modules/FiberEldyn/src"
+          "/home/ivan/data/gdrive/science/research/projects/julia-modules/FiberEldyn/src"
          )
-elseif gethostname() == "core"
+else
     push!(
           LOAD_PATH, 
-          "/home/ivan/data/gdrive/science/research/projects/julia-modules/FiberEldyn/src"
+          "/home/ivan/gdrive/science/research/projects/julia-modules/FiberEldyn/src"
          )
 end
 
