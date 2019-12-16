3a3b9697070918e5ec86982b4c230868ea02b890
Ivan <toftul.ivan@gmail.com>
2019-12-16 16:11:25 +0300

    Changes to be committed: 	modified:   .ipython/profile_default/startup/00_load_sympy.py

--- .ipython/profile_default/startup/00_load_sympy.py
+++ .ipython/profile_default/startup/00_load_sympy.py
@@ -1,5 +1,6 @@
 from sympy import *
 init_printing()
 
-x, y, z, t, theta, phi, gamma, beta = symbols("x y z t theta phi gamma beta")
-i j n m = symbols("i j n m", integer=True)
+x, y, z, t = symbols("x y z t")
+theta, phi, gamma, beta = symbols("theta phi gamma beta")
+i, j, n, m = symbols("i j n m", integer=True)
